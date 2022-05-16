b2c4338594478b96aae9f7a3c1356c2df3480138
Frank Weijers <frank@weijers.io>
2022-05-16 10:02:22 +0200

    Export BulkMutationError

--- src/mutation/index.ts
+++ src/mutation/index.ts
@@ -1,3 +1,3 @@
-export { BulkMutation } from './bulk-mutation';
+export { BulkMutation, BulkMutationError } from './bulk-mutation';
 export { BulkMutationWithUploads } from './bulk-mutation-with-uploads';
 export { PointInput } from './dto/point.input';
