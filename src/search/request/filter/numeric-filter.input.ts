9df18b7f821ac615bbf06242177ddfb879c8d8b4
Frank Weijers <frank@weijers.io>
2022-06-03 13:20:24 +0200

    Set types for NumericFilterInput to Int

--- src/search/request/filter/numeric-filter.input.ts
+++ src/search/request/filter/numeric-filter.input.ts
@@ -1,29 +1,29 @@
-import { Field, InputType } from '@nestjs/graphql';
+import { Field, InputType, Int } from '@nestjs/graphql';
 import { NumericFilter } from '@appvise/domain';
 
 @InputType('NumericFilter')
 export class NumericFilterInput implements NumericFilter {
-  @Field(() => Number, {
+  @Field(() => Int, {
     nullable: true,
   })
   equals?: number;
 
-  @Field(() => Number, {
+  @Field(() => Int, {
     nullable: true,
   })
   lt?: number;
 
-  @Field(() => Number, {
+  @Field(() => Int, {
     nullable: true,
   })
   lte?: number;
 
-  @Field(() => Number, {
+  @Field(() => Int, {
     nullable: true,
   })
   gt?: number;
 
-  @Field(() => Number, {
+  @Field(() => Int, {
     nullable: true,
   })
   gte?: number;
